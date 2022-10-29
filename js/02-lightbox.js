0a20685db15a9b4e2e77a22b3246fb00f22b0355
onluwin <nikitagurzhiy1@gmail.com>
2022-10-30 00:43:46 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,22 +5,11 @@ import { galleryItems } from './gallery-items.js';
 const galleryList = document.querySelector('.gallery');
 const galleryItem = document.querySelector('.gallery__item');
 
-
 const markup = galleryItems.map
     (item => {
     return `<li><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a></li>`
     } ).join('')
 galleryList.innerHTML = markup
-
-// galleryList.addEventListener('click', onGalleryItemClick) 
-// function onGalleryItemClick(e) {
-//     e.preventDefault()
-//     if (e.target.nodeName !== 'IMG') {
-//         return
-//     }
-    
-// }
-
 
 let gallery = new SimpleLightbox('.gallery a', {
     captionType: 'attr',
